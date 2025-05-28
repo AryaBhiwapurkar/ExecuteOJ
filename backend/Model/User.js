ab5f1d953cc546ed5623e8e189a23a818807f30c
Arya Bhiwapurkar <aaryanspl@gmail.com>
2025-05-28 18:37:02 +0530

    Initial commit, done with frontend and backend of the login and register setup

---
+++ backend/Model/User.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    firstName:{
+        type: String,
+        default:null,
+        required: true,
+        
+        
+    },
+    lastName:{
+        type: String,
+        default:null,
+        required: true,
+        
+    },
+    email:{
+        type: String,
+        default:null,
+        required: true,
+        unique:true,
+        
+    },
+    password:{
+        type: String,
+        required: true,
+        
+    }
+    
+});
+module.exports = mongoose.model('user', userSchema); //(Name, Schema) creates a model for the User collection in MongoDB.
